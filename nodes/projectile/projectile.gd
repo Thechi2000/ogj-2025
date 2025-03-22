370587750c69e006271a2664f014436d2138ccf4
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-03-22 20:51:07 +0100

    refactor: modularize projectile behavior

--- nodes/projectile/projectile.gd
+++ nodes/projectile/projectile.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var DAMAGE : float = 0.0
 @export var direction := Vector2()
 
+func _on_hit():
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	if self.move_and_collide(direction * delta):
-		queue_free()
+		_on_hit()
