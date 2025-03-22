157123bd2c5e6431626c473b0323b0d886348a37
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-03-22 23:16:13 +0100

    feat: add windup time to sword

--- nodes/modules/sword/sword.gd
+++ nodes/modules/sword/sword.gd
@@ -7,13 +7,13 @@ var _active = false
 func cooldown():
 	return 1
 
+func _ready():
+	hide()
+
 func _use():
-	_active = true
 	$AnimatedSprite2D.play()
-	$ActiveTimer.start()
-	
-	for body in ($"." as Area2D).get_overlapping_bodies():
-		_on_body_entered(body)
+	$WindupTimer.start()
+	show()
 
 func _on_body_entered(body: Node2D) -> void:
 	if _active and body.has_method("update_health"):
@@ -21,4 +21,12 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_active_timer_timeout() -> void:
 	$AnimatedSprite2D.stop()
+	hide()
 	_active = false
+
+func _on_windup_timer_timeout() -> void:
+	$ActiveTimer.start()
+	_active = true
+
+	for body in ($"." as Area2D).get_overlapping_bodies():
+		_on_body_entered(body)
