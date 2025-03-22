92a94b8e9871e7ff8f1d7ea0cc92f9dc15a7ac8e
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-03-22 17:31:06 +0100

    fix: make player CharacterBody

--- nodes/player/player.gd
+++ nodes/player/player.gd
@@ -1,5 +1,5 @@
 class_name Player
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 200 # How fast the player will move (pixels/sec).
 @export var hud: HUD
@@ -27,7 +27,7 @@ func remove_module(slot: ModuleSlot):
 		modules.erase(slot)
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -51,7 +51,7 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		
-	position += velocity * delta
+	move_and_slide()
 	$AnimatedSprite2D.flip_h = velocity.x < 0
 
 func update_health(diff):
