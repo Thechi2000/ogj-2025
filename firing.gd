ff3036ebff665f4eb2f2e4870c0a99a41831d56b
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-03-22 19:24:43 +0100

    refactor: remove unused scripts

--- firing.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		$Gun.fire()
-	if Input.is_action_pressed("ui_down"):
-		$Gun.rotation_degrees += 20.0*delta
-	if Input.is_action_pressed("ui_up"):
-		$Gun.rotation_degrees -= 20.0*delta
