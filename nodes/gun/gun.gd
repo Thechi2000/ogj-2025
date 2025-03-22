410ec211373a7be85c02a3f8e213e9b2a7f895c6
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-03-22 17:15:48 +0100

    fix: gun color origin and rotation

--- nodes/gun/gun.gd
+++ nodes/gun/gun.gd
@@ -7,6 +7,6 @@ func fire():
 	var new_projectile = preload("res://nodes/projectile/projectile.tscn").instantiate()
 	new_projectile.DAMAGE = DAMAGE
 	new_projectile.direction = FIRING_DIR.rotated(self.rotation)
-	new_projectile.rotation = self.rotation
-	new_projectile.position = self.position
+	new_projectile.rotation = self.global_rotation
+	new_projectile.position = self.global_position
 	get_tree().current_scene.add_child(new_projectile)
