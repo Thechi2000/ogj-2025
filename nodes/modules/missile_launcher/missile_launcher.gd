c3c9f9b0c9c3a02aeb9594b7d6942c9ffce2a729
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-03-23 07:52:14 +0100

    feat: add marker to correctly launch projectiles

--- nodes/modules/missile_launcher/missile_launcher.gd
+++ nodes/modules/missile_launcher/missile_launcher.gd
@@ -10,5 +10,5 @@ func _use():
 	var new_projectile = preload("res://nodes/modules/missile_launcher/missile/missile.tscn").instantiate()
 	new_projectile.direction = FIRING_DIR.rotated(self.rotation)
 	new_projectile.rotation = self.global_rotation
-	new_projectile.position = self.global_position
+	new_projectile.position = $LCannon.global_position if is_left_module else $RCannon.global_position
 	get_tree().current_scene.add_child(new_projectile)
